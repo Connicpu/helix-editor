552d1a6f7c45d9a6a3cdb7697ef5c3226a8accbd
Connie Hilarides <Connie.Hilarides@spacex.com>
2025-05-05 10:51:43 -0700

    Integrate master

--- helix-vcs/src/git/blame.rs
+++ helix-vcs/src/git/blame.rs
@@ -65,12 +65,16 @@ impl FileBlame {
                 .and_then(|c| c.short_id().map(|id| id.to_string()).ok()),
             author_name: author.map(|a| a.name.to_string()),
             author_email: author.map(|a| a.email.to_string()),
-            commit_date: author.map(|a| a.time.format(gix::date::time::format::SHORT)),
+            commit_date: author
+                .and_then(|a| a.time().ok())
+                .map(|t| t.format(gix::date::time::format::SHORT)),
             commit_message: message.as_ref().map(|msg| msg.title.to_string()),
             commit_body: message
                 .as_ref()
                 .and_then(|msg| msg.body.map(|body| body.to_string())),
-            time_stamp: author.map(|a| (a.time.seconds, a.time.offset)),
+            time_stamp: author
+                .and_then(|a| a.time().ok())
+                .map(|t| (t.seconds, t.offset)),
             time_ago: None,
         };
 
